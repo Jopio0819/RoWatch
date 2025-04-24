462d9d4a269d50f585864a9565d74fea87a33d29
Jopio0819 <113040450+Jopio0819@users.noreply.github.com>
2025-04-24 08:13:37 +0200

    Create Config.lua

---
+++ game1/ReplicatedStorage/Config.lua
@@ -0,0 +1,13 @@
+local config = {}
+
+
+function config.WhitelistCode(plr)
+		return true -- RETURN TRUE TO MAKE THE USER A MODERATOR
+end
+
+config.AllowedPlaces = {
+	17492614304, -- Set all the allowed places here!
+	17479858439
+}
+
+return config
